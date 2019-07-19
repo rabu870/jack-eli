1161a0b65bbf82c98a9456820a9bd81f785d2333
Eli Kimchi <rabu8770@gmail.com>
2019-07-19 11:26:33 -0700

    prevent merge conflicts
    Co-Authored-By: jacknote <jacknote@users.noreply.github.com>

---
+++ inc/Enemy.hpp
@@ -0,0 +1,23 @@
+#ifndef __ENEMY_HPP
+#define __ENEMY_HPP
+
+#include "Object.hpp"
+#include "Graphics.hpp"
+
+class enemy : public Aspen::Object::Object
+{
+    unsigned int currentNode = 0;
+    Aspen::Graphics::Animation *forward;
+    Aspen::Graphics::Animation *backwards;
+    Aspen::Graphics::Animation *death;
+    float arrayX[20] = {40.0f, 82.0f, 118.0f, 172.0f, 728.0f, 801.0f, 1036.0f, 1090.0f, 1117.0f, 1078.0f, 966.0f, 916.0f, 873.0f, 780.0f, 665.0f, 640.0f, 540.0f, 255.0f, 191.0f, -35.0f};
+    float arrayY[20] = {356.0f, 405.0f, 531.0f, 568.0f, 568.0f, 512.0f, 507.0f, 478.0f, 409.0f, 314.0f, 282.0f, 230.0f, 117.0f, 57.0f, 137.0f, 239.0f, 289.0f, 289.0f, 235.0f, 235.0f};
+
+public:
+    int health = 1001;
+    enemy(Object *parent = nullptr, std::string name = "enemy");
+    void OnUpdate();
+    void TakeDamage(int damage);
+}
+
+#endif
--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -9,60 +9,60 @@
 #include "Audio.hpp"
 #include "Input.hpp"
 #include "GameState.hpp"
+#include "Enemy.hpp"
 #include <iostream>
 #include <math.h>
 
-class enemy : public Aspen::Object::Object
+enemy::enemy(Object *parent = nullptr, std::string name = "enemy") : Aspen::Object::Object(parent, name)
 {
-    unsigned int currentNode = 0;
-    Aspen::Graphics::Animation *forward;
-    Aspen::Graphics::Animation *backwards;
-    Aspen::Graphics::Animation *death;
-    float arrayX[20] = {40.0f, 82.0f, 118.0f, 172.0f, 728.0f, 801.0f, 1036.0f, 1090.0f, 1117.0f, 1078.0f, 966.0f, 916.0f, 873.0f, 780.0f, 665.0f, 640.0f, 540.0f, 255.0f, 191.0f, -35.0f};
-    float arrayY[20] = {356.0f, 405.0f, 531.0f, 568.0f, 568.0f, 512.0f, 507.0f, 478.0f, 409.0f, 314.0f, 282.0f, 230.0f, 117.0f, 57.0f, 137.0f, 239.0f, 289.0f, 289.0f, 235.0f, 235.0f};
+    CreateChild<Aspen::Transform::Transform>();
+    forward = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/sprites/bat/forward-lg.png", 16 * 8, 24 * 8, 5, nullptr, "BatFwd"), 1.0f / 10.0f, this, "Animation");
+    backwards = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/sprites/bat/backwards-lg.png", 16 * 8, 24 * 8, 5, nullptr, "BatBckd"), 1.0f / 10.0f, this, "Animation");
+    death = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/sprites/bat/death-lg.png", 16 * 8, 24 * 8, 5, nullptr, "BatDeath"), 1.0f / 10.0f, this, "Animation");
+    AddChild(forward);
+    AddChild(backwards);
+    AddChild(death);
+    backwards->Deactivate();
+    death->Deactivate();
+    GetTransform()->SetScale(.25, .25);
+    GetTransform()->SetPosition(-170, 1437);
+    CreateChild<Aspen::Physics::Rigidbody>();
+    CreateChild<Aspen::Physics::CircleCollider>()->SetRadius(16);
+}
 
-public:
-    enemy(Object *parent = nullptr, std::string name = "enemy") : Aspen::Object::Object(parent, name)
+void enemy::OnUpdate()
+{
+    if (currentNode < sizeof(arrayY) / sizeof(float) && currentNode < sizeof(arrayX) / sizeof(float))
     {
-        CreateChild<Aspen::Transform::Transform>();
-        forward = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/sprites/bat/forward-lg.png", 16 * 8, 24 * 8, 5, nullptr, "BatFwd"), 1.0f / 10.0f, this, "Animation");
-        backwards = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/sprites/bat/backwards-lg.png", 16 * 8, 24 * 8, 5, nullptr, "BatBckd"), 1.0f / 10.0f, this, "Animation");
-        death = new Aspen::Graphics::Animation(new Aspen::Graphics::UniformSpritesheet("./resources/sprites/bat/death-lg.png", 16 * 8, 24 * 8, 5, nullptr, "BatDeath"), 1.0f / 10.0f, this, "Animation");
-        AddChild(forward);
-        AddChild(backwards);
-        AddChild(death);
-        backwards->Deactivate();
-        death->Deactivate();
-        GetTransform()->SetScale(.25, .25);
-        GetTransform()->SetPosition(-170, 1437);
-        CreateChild<Aspen::Physics::Rigidbody>();
-        CreateChild<Aspen::Physics::CircleCollider>()->SetRadius(16);
-    }
+        float dy = arrayY[currentNode] - GetTransform()->GetYPosition();
+        float dx = arrayX[currentNode] - GetTransform()->GetXPosition();
 
-    void OnUpdate()
-    {
-        if (currentNode < sizeof(arrayY) / sizeof(float) && currentNode < sizeof(arrayX) / sizeof(float))
+        GetRigidbody()->SetCartesianVelocity(dx, dy);
+        GetRigidbody()->SetVelocityStrength(.6 * 6);
+        // if (GetTransform()->GetXPosition() == arrayX[currentNode] && GetTransform()->GetYPosition() == arrayY[currentNode])
+        float r = 20;
+        Aspen::Log::Debug("%f, %f", dx, dy);
+        if (dy * dy + dx * dx < r * r)
         {
-            float dy = arrayY[currentNode] - GetTransform()->GetYPosition();
-            float dx = arrayX[currentNode] - GetTransform()->GetXPosition();
-
-            GetRigidbody()->SetCartesianVelocity(dx, dy);
-            GetRigidbody()->SetVelocityStrength(.6 * 6);
-            // if (GetTransform()->GetXPosition() == arrayX[currentNode] && GetTransform()->GetYPosition() == arrayY[currentNode])
-            float r = 20;
-            Aspen::Log::Debug("%f, %f", dx, dy);
-            if (dy * dy + dx * dx < r * r)
+            Aspen::Log::Debug("Close!");
+            if (currentNode == 8)
             {
-                Aspen::Log::Debug("Close!");
-                if (currentNode == 8)
-                {
-                    forward->Deactivate();
-                    backwards->Activate();
-                }
-                currentNode++;
+                forward->Deactivate();
+                backwards->Activate();
             }
+            currentNode++;
         }
-        else
-            GetRigidbody()->SetVelocityStrength(0);
     }
-};
+    else
+        GetRigidbody()->SetVelocityStrength(0);
+}
+
+void enemy::TakeDamage(int damage)
+{
+    health -= damage;
+
+    if (health <= 0)
+    {
+        End();
+    }
+}
